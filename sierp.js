4528c2ba7f199c3f2d1188f464a56f1e64817e88
shipa99 <40608301+shipa99@users.noreply.github.com>
2018-10-26 22:29:48 +0300

    Update sierp.js

--- sierp.js
+++ sierp.js
@@ -1,34 +1,45 @@
+// Set Canvas
 var canv 	= document.getElementById('canvas'),
 	ctx 	= canv.getContext('2d');
 
+// Set context
 canv.width = window.innerWidth;
 canv.height = window.innerHeight;
-ctx.lineWidth = 2;
+ctx.strokeStyle = 'black';
 
-var x0 = 700, y0 = 350;
+// Set background
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+
+// Set points
+var x0 = 50, y0 = 700;
+
 ctx.beginPath();
 ctx.moveTo(x0, y0);
 
 var x_cur = x0, y_cur = y0;
 var cur_angle = 0;
+cur_angle = cur_angle + (Math.PI/4);
 
 function SierpRight(depth, step){
 	if (depth > 0){
 		depth = depth - 1;
 
 		SierpRight(depth, step);
-		cur_angle = cur_angle - (Math.Pi/4);
 		ctx.lineTo(x_cur + step * Math.cos(cur_angle), y_cur - step * Math.cos(cur_angle));
+		ctx.stroke();
 		x_cur = x_cur + step * Math.cos(cur_angle);
 		y_cur = y_cur - step * Math.cos(cur_angle);
 
 		SierpDown(depth, step);
 		ctx.lineTo(x_cur + 2 * step, y_cur);
+		ctx.stroke();
 		x_cur = x_cur + step * 2;
+		y_cur = y_cur;
 
-		SierpUp(depth, step);
-		cur_angle = cur_angle + (Math.Pi/4);	
+		SierpUp(depth, step);	
 		ctx.lineTo(x_cur + step * Math.cos(cur_angle), y_cur + step * Math.cos(cur_angle));
+		ctx.stroke();
 		x_cur = x_cur + step * Math.cos(cur_angle);
 		y_cur = y_cur + step * Math.cos(cur_angle);
 
@@ -38,21 +49,23 @@ function SierpRight(depth, step){
 
 function SierpDown(depth, step){
 	if (depth > 0){
-		depth--;
+		depth = depth - 1;
 
 		SierpDown(depth, step);
-		cur_angle = cur_angle - (Math.Pi*3/4);
 		ctx.lineTo(x_cur - step * Math.cos(cur_angle), y_cur - step * Math.cos(cur_angle));
+		ctx.stroke();
 		x_cur = x_cur - step * Math.cos(cur_angle);
 		y_cur = y_cur - step * Math.cos(cur_angle);
 
 		SierpLeft(depth, step);
 		ctx.lineTo(x_cur, y_cur - 2 * step);
+		ctx.stroke();
+		x_cur = x_cur;
 		y_cur = y_cur - step * 2;
 
 		SierpRight(depth, step);
-		cur_angle = cur_angle - (Math.Pi/4);	
 		ctx.lineTo(x_cur + step * Math.cos(cur_angle), y_cur - step * Math.cos(cur_angle));
+		ctx.stroke();
 		x_cur = x_cur + step * Math.cos(cur_angle);
 		y_cur = y_cur - step * Math.cos(cur_angle);
 
@@ -62,21 +75,22 @@ function SierpDown(depth, step){
 
 function SierpUp(depth, step){
 	if (depth > 0){
-		depth--;
+		depth = depth - 1;
 
 		SierpUp(depth, step);
-		cur_angle = cur_angle + (Math.Pi/4);
 		ctx.lineTo(x_cur + step * Math.cos(cur_angle), y_cur + step * Math.cos(cur_angle));
+		ctx.stroke();
 		x_cur = x_cur + step * Math.cos(cur_angle);
 		y_cur = y_cur + step * Math.cos(cur_angle);
 
 		SierpRight(depth, step);
-		ctx.lineTo(y_cur + 2 * step, y_cur);
-		y_cur = x_cur + step * 2;
+		ctx.lineTo(x_cur, y_cur + step * 2);
+		ctx.stroke();
+		y_cur = y_cur + step * 2;
 
-		SierpLeft(depth, step);
-		cur_angle = cur_angle + (Math.Pi*3/4);	
+		SierpLeft(depth, step);	
 		ctx.lineTo(x_cur - step * Math.cos(cur_angle), y_cur + step * Math.cos(cur_angle));
+		ctx.stroke();
 		x_cur = x_cur - step * Math.cos(cur_angle);
 		y_cur = y_cur + step * Math.cos(cur_angle);
 
@@ -86,21 +100,22 @@ function SierpUp(depth, step){
 
 function SierpLeft(depth, step){
 	if (depth > 0){
-		depth--;
+		depth = depth - 1;
 
-		SierpLeft(depth, step);
-		cur_angle = cur_angle + (Math.Pi*3/4);	
+		SierpLeft(depth, step);	
 		ctx.lineTo(x_cur - step * Math.cos(cur_angle), y_cur + step * Math.cos(cur_angle));
+		ctx.stroke();
 		x_cur = x_cur - step * Math.cos(cur_angle);
 		y_cur = y_cur + step * Math.cos(cur_angle);
 
 		SierpUp(depth, step);
 		ctx.lineTo(x_cur - 2 * step, y_cur);
+		ctx.stroke();
 		x_cur = x_cur - step * 2;
 
 		SierpDown(depth, step);
-		cur_angle = cur_angle + (Math.Pi*5/4);	
 		ctx.lineTo(x_cur - step * Math.cos(cur_angle), y_cur - step * Math.cos(cur_angle));
+		ctx.stroke();
 		x_cur = x_cur - step * Math.cos(cur_angle);
 		y_cur = y_cur - step * Math.cos(cur_angle);
 
@@ -108,6 +123,37 @@ function SierpLeft(depth, step){
 	}
 }
 
-SierpRight(3, 20);
+function Sierpinski(depth, step){
+	if (depth > 0){
+		depth = depth - 1;
+
+		SierpRight(depth, step);
+		ctx.lineTo(x_cur + step * Math.cos(cur_angle), y_cur - step * Math.cos(cur_angle));
+		ctx.stroke();
+		x_cur = x_cur + step * Math.cos(cur_angle);
+		y_cur = y_cur - step * Math.cos(cur_angle);
+
+		SierpDown(depth, step);
+		ctx.lineTo(x_cur - step * Math.cos(cur_angle), y_cur - step * Math.cos(cur_angle));
+		ctx.stroke();
+		x_cur = x_cur - step * Math.cos(cur_angle);
+		y_cur = y_cur - step * Math.cos(cur_angle);
+
+		SierpLeft(depth, step);
+		ctx.lineTo(x_cur - step * Math.cos(cur_angle), y_cur + step * Math.cos(cur_angle));
+		ctx.stroke();
+		x_cur = x_cur - step * Math.cos(cur_angle);
+		y_cur = y_cur + step * Math.cos(cur_angle);
+
+		SierpUp(depth, step);
+		ctx.lineTo(x_cur + step * Math.cos(cur_angle), y_cur + step * Math.cos(cur_angle));
+		ctx.stroke();
+		x_cur = x_cur + step * Math.cos(cur_angle);
+		y_cur = y_cur + step * Math.cos(cur_angle);
+
+	}
+}
+
+Sierpinski(4, 20);
 
 ctx.closePath();
